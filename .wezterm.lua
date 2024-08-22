d620395b62f4048211854be4d2eccd200995c881
dox <doxtech@doxs-MacBook-Pro.local>
2024-08-22 06:57:01 +0300

    make my tmux start with wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -17,6 +17,6 @@ config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.87
 config.macos_window_background_blur = 9
-
+config.default_prog = { "tmux" }
 -- and finally, return the configuration to wezterm
 return config
