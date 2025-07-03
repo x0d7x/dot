7ed63df92ccf0e46c6c01b3573a7dcbafbe5857e
d7manDev <a.bakider@gmail.com>
2025-07-03 16:14:48 +0300

    chore : update the sticky script

--- .config/scripts/sticky.sh
+++ .config/scripts/sticky.sh
@@ -4,8 +4,14 @@
 # The application name to make sticky
 APP_NAME="OBS Studio"
 
-# Get the focused workspace name
-FOCUSED_WORKSPACE=$AEROSPACE_FOCUSED_WORKSPACE
+# Get the window ID for the application
+WINDOW_ID=$(aerospace list-windows --all | awk -v app="$APP_NAME" '$0 ~ app {print $1}')
 
-# Move the application to the focused workspace
-aerospace move-node-to-workspace $FOCUSED_WORKSPACE --window-id $(aerospace list-windows --all | awk -v app="$APP_NAME" '$0 ~ app {print $1}')
+# If a window ID was found, move the application to the focused workspace
+if [ -n "$WINDOW_ID" ]; then
+  # Get the focused workspace name
+  FOCUSED_WORKSPACE=$AEROSPACE_FOCUSED_WORKSPACE
+
+  # Move the application to the focused workspace
+  aerospace move-node-to-workspace $FOCUSED_WORKSPACE --window-id $WINDOW_ID
+fi
