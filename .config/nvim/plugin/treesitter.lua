0d506acb70879a94e3e31e07a6a0f856aa2f876e
x0d7x <75843278+x0d7x@users.noreply.github.com>
2026-04-06 01:56:59 +0300

    fix(treesitter): treesitter work in nvim 0.12

--- .config/nvim/plugin/treesitter.lua
+++ .config/nvim/plugin/treesitter.lua
@@ -1,44 +1,71 @@
 local pack = require("core.pack")
 
 pack.add({
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 })
 
 local ensure_installed = {
-    "bash", "c", "html", "javascript", "json", "toml", "lua", "luadoc", "luap",
-    "markdown", "markdown_inline", "python", "regex", "tsx", "typescript", "vue",
-    "vim", "vimdoc", "yaml", "rust", "go", "gomod", "gowork", "gosum", "php", "astro",
+	"bash",
+	"c",
+	"html",
+	"javascript",
+	"json",
+	"toml",
+	"lua",
+	"luadoc",
+	"luap",
+	"markdown",
+	"markdown_inline",
+	"python",
+	"regex",
+	"tsx",
+	"typescript",
+	"vue",
+	"vim",
+	"vimdoc",
+	"yaml",
+	"rust",
+	"go",
+	"gomod",
+	"gowork",
+	"gosum",
+	"php",
+	"astro",
 }
 
+-- Install parsers on buffer enter
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "*",
-    callback = function(args)
-        pcall(vim.treesitter.start)
-        vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-    end,
+	pattern = "*",
+	callback = function(args)
+		if not args.filetype or args.filetype == "" then
+			return
+		end
+
+		local lang = vim.treesitter.language.get_lang(args.filetype)
+		if not lang then
+			return
+		end
+
+		local ts = require("nvim-treesitter")
+		ts.install(lang)
+		vim.defer_fn(function()
+			vim.treesitter.start()
+		end, 100)
+		vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
+		local win = vim.fn.bufwinid(args.buf)
+		if win > 0 then
+			vim.wo[win].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+			vim.wo[win].foldmethod = "expr"
+		end
+	end,
 })
 
+-- Install ensure_installed parsers on startup
 vim.api.nvim_create_autocmd("User", {
-    pattern = "PackChanged",
-    callback = function(args)
-        local kind = args.data and args.data.kind
-        if kind ~= "install" and kind ~= "update" then
-            return
-        end
-
-        local name = args.data.spec and args.data.spec.name
-        if name ~= "nvim-treesitter" then
-            return
-        end
-
-        local ts = require("nvim-treesitter")
-        local installed = ts.config.get_installed()
-        local to_install = vim.tbl_filter(function(parser)
-            return not vim.tbl_contains(installed, parser)
-        end, ensure_installed)
-
-        if #to_install > 0 then
-            ts.install(to_install)
-        end
-    end,
+	pattern = "VeryLazy",
+	callback = function()
+		local ts = require("nvim-treesitter")
+		ts.install(ensure_installed)
+	end,
 })
